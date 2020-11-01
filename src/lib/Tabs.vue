20f0e96ea3b2192e3ad5c2a22d0201a49c982065
GQiangQ <740407980@qq.com>
2020-11-01 23:39:58 +0800

    tab完成

---
+++ src/lib/Tabs.vue
@@ -0,0 +1,131 @@
+<template>
+<div class="g-tabs">
+  <!-- 头部 -->
+  <div class="g-tabs-nav" ref="tabNav">
+    <div class="g-tabs-nav-item"
+      v-for="(item,index) in title" 
+      :ref="bindRef" 
+      :key="index" 
+      @click="select(item.value)"  
+      :class="{'g-tabs-nav-item-selected':selected === item.value}" 
+      :value="item.value"
+    >
+      {{item.label}}
+    </div>
+    <div ref="selectedLine" class="g-tabs-nav-line" />
+  </div>
+  <!-- 显示的内容 -->
+  <div class="g-tabs-content">
+      <component class="g-tabs-content-item" :is="selectedItemContent" :key="selectedItemContent.props.value"/>
+  </div> 
+</div> 
+</template>
+<script lang="ts">
+import { onMounted, onUpdated,watchEffect, ref, computed } from 'vue'
+import Tab from './Tab.vue'
+export default{
+  name:'tabs',
+  props:{
+    selected: {
+      type: String,
+    },
+  },
+  setup (props, content) {
+    const selectedItem = ref<HTMLDivElement>(null) // 选中元素的ref
+    const selectedLine = ref<HTMLDivElement>(null) // 选中下划线的ref
+    const tabNav = ref<HTMLDivElement>(null) // nav容器
+   
+    // 生命周期执行的内容
+    onMounted(()=>{
+      console.log('onMounted:')
+      watchEffect(() => {
+        console.log('根性了',selectedItem.value)
+        const {width} = selectedItem.value.getBoundingClientRect()
+        selectedLine.value.style.width = width + 'px'
+        const {left: left1} = selectedItem.value.getBoundingClientRect()
+        const {left: left2} = tabNav.value.getBoundingClientRect()
+        const left = left1 - left2
+        selectedLine.value.style.left = left + 'px'
+      })
+    })
+    // 加了这个才可以实时选中
+    // onUpdated(()=>{
+    //     const {width} = selectedItem.value.getBoundingClientRect()
+    //     selectedLine.value.style.width = width + 'px'
+    //     const {left: left1} = selectedItem.value.getBoundingClientRect()
+    //     const {left: left2} = tabNav.value.getBoundingClientRect()
+    //     const left = left1 - left2
+    //     selectedLine.value.style.left = left + 'px'
+    // })
+    let title = []  // title集合
+    const defaults = content.slots.default()// 显示的内容
+    defaults.forEach(item => {
+      title.push({
+        label:item.props.title,
+        value:item.props.value
+      })
+      if(item && item.type !== Tab){
+        throw new Error('Tabs 子标签必须是Tab')
+      }
+    })
+
+    // 触发点击事件跟新代码
+    const select = (value) => {
+      content.emit('update:selected', value)
+    }
+
+    // 绑定选中的ref
+    const bindRef = (el:HTMLDivElement) => {
+      if(el?.getAttribute('value') === props.selected){
+        selectedItem.value = el
+      }
+    }
+    // 计算选中的值
+    const selectedItemContent = computed(() => {
+      console.log('change')
+      return defaults.find(item => item.props.value === props.selected)
+    })
+    return {
+      defaults, title, select, bindRef, selectedLine ,tabNav, selectedItemContent
+    }
+  }  
+}
+</script>
+<style lang="scss">
+.g-tabs{
+  &-nav{
+    display: flex;
+    position: relative;
+    border-bottom: 1px solid #dad8d8;
+    &-line{
+      bottom: -1px;
+      position: absolute;
+      width: 100%;
+      background: #409eff;
+      height: 2px;
+      left: 0;
+      border-radius: 2px;
+      transition: all 250ms;
+    }
+    &-item{
+      &-selected{
+        color: #409eff;
+      }
+      height: 40px;
+      line-height: 40px;
+      text-align: center;
+      cursor: pointer;
+      &:hover{
+        color: #409eff;
+      }
+      &:first-child{
+        margin-left: 0;
+      }
+      margin:0 1rem;
+    }
+    &:last-child{
+      margin-right: 0;
+    }
+  }
+}
+</style>
