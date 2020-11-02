12bcde7a3867efdfb4cd3242fd74ae5d37fdabe3
GQiangQ <740407980@qq.com>
2020-11-02 23:06:20 +0800

    github-markdown-css

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './index.scss'
+import 'github-markdown-css'
+
 import { router } from './router';
 createApp(App).use(router).mount('#app')
