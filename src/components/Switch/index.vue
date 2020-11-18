edb2b00b2bf252e795b99b09193dcc15f14af68b
GQiangQ <740407980@qq.com>
2020-11-18 22:23:50 +0800

    修改switch的api介绍

--- src/components/Switch/index.vue
+++ src/components/Switch/index.vue
@@ -2,6 +2,7 @@
 <h1>Switch 组件示例</h1>
   <card :component="demo1" :describe='md1' />
   <card :component="demo2" :describe='md2' />
+  <div v-html="use"></div>
 </template>
 <script lang="ts">
 import { computed, ref } from 'vue'
@@ -11,12 +12,14 @@ import demo1 from './components/demo1.vue'
 import demo2 from './components/demo2.vue'
 import md1 from './components/demo1.md'
 import md2 from './components/demo2.md'
+import use from './use.md'
+
 export default {
   components:{
     card, Alert
   },
   setup(){
-    return { demo1, demo2, md1, md2}
+    return { demo1, demo2, md1, md2, use}
   }
 }
 </script>
