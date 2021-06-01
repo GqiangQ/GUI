3a2eec19e2b51893d5bc1820eab0c57ddc80b6bf
qqiang-g <740407980@qq.com>
2021-06-01 23:33:08 +0800

    feat: 升级包版本

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -3,3 +3,16 @@ declare module '*.vue' {
   const componentOptions: ComponentOptions
   export default componentOptions
 }
+declare module "*.md" {
+  const value: any;
+  export default value;
+}
+// declare module "*.vue" {
+//   import Vue from 'vue'
+//   export default Vue
+// }
+
+// declare module "*.md" {
+//   import Vue from 'vue'
+//   export default Vue
+// }
